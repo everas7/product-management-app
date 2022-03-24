0f2b3ae78106ffc6a6f858552f2f4f797dbe9f3b
Enmanuel Veras <enmaveras07@gmail.com>
2022-03-24 18:05:28 -0400

    adds more demo users

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
 # product-management-app
+
+## Instructions on How To Run
+```
+cd api
+cp .env.example .env
+docker-compose up
+```
+
+```
+cd webapp
+cp .env.example .env
+npm run start
+```
+
+## Demo Users and Roles
+- Only users with Authority PRODUCT_CREATORS can create products.
+- Only users with Authority PRODUCT_MANAGERS can modify and/or delete products.
+- Only users with the ROLE PRODUCT_PRICING can manage or modify product prices.
+
+All users has same password: strongpassword1
+
+- John Doe
+    - Roles: All Roles
+    - Email: jdoe@test.com
+
+- Jack Sparrow
+    - Roles: PRODUCT_PRICING
+    - Email: jsparrow@test.com
+
+- Clark Kent
+    - Roles: PRODUCT_MANAGERS
+    - Email: ckent@test.com
+
+- Peter Parker
+    - Roles: PRODUCT_CREATORS
+    - Email: pparker@test.com
--- api/src/db/seeders/20220324140000-demo-users.js
+++ api/src/db/seeders/20220324140000-demo-users.js
@@ -1,21 +1,37 @@
-'use strict';
-const bcrypt = require('bcrypt');
+"use strict";
+const bcrypt = require("bcrypt");
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    return queryInterface.bulkInsert('users', [
+    return queryInterface.bulkInsert("users", [
       {
-        name: 'John Doe',
-        email: 'jdoe@test.com',
-        roles: JSON.stringify(['PRODUCT_CREATORS', 'PRODUCT_MANAGERS']),
-        password: await bcrypt.hash('strongpassword1', 10),
+        name: "John Doe",
+        email: "jdoe@test.com",
+        roles: JSON.stringify(["PRODUCT_CREATORS", "PRODUCT_MANAGERS"]),
+        password: await bcrypt.hash("strongpassword1", 10),
         createdAt: new Date(),
         updatedAt: new Date(),
       },
       {
-        name: 'Jack Sparrow',
-        email: 'jsparrow@test.com',
-        roles: JSON.stringify(['PRODUCT_PRICING']),
-        password: await bcrypt.hash('strongpassword1', 10),
+        name: "Jack Sparrow",
+        email: "jsparrow@test.com",
+        roles: JSON.stringify(["PRODUCT_PRICING"]),
+        password: await bcrypt.hash("strongpassword1", 10),
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: "Clark Kent",
+        email: "ckent@test.com",
+        roles: JSON.stringify(["PRODUCT_MANAGERS"]),
+        password: await bcrypt.hash("strongpassword1", 10),
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: "Peter Parker",
+        email: "pparker@test.com",
+        roles: JSON.stringify(["PRODUCT_CREATORS"]),
+        password: await bcrypt.hash("strongpassword1", 10),
         createdAt: new Date(),
         updatedAt: new Date(),
       },
@@ -23,6 +39,6 @@ module.exports = {
   },
 
   down: async (queryInterface, Sequelize) => {
-    return queryInterface.bulkDelete('users', null, {});
+    return queryInterface.bulkDelete("users", null, {});
   },
 };
